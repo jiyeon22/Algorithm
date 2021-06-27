c5a9f12511a3ba31a8ca60e14ceaf5ea361ceadf
jiyeon22 <kkll4106@naver.com>
2021-06-28 05:50:22 +0900

    list

--- list.py
+++ list.py
@@ -34,3 +34,16 @@ print(new_list)
 
 numbers.sort() #sort정렬 : 아무것도 리턴하지 않고, 기존 리스트를 정렬
 print(numbers)
+
+# 화씨 온도에서 섭씨 온도로 바꿔 주는 함수
+def fahrenheit_to_celsius(fahrenheit):
+    return (fahrenheit - 32) * 5 / 9
+
+temperature_list = [40, 15, 32, 64, -4, 11]
+print("화씨 온도 리스트: " + str(temperature_list))  # 화씨 온도 출력
+
+i = 0
+while i < len(temperature_list):
+    temperature_list[i] = round(fahrenheit_to_celsius(temperature_list[i]), 1) #라운드함수 >반올림 (소수점1째자리)
+    i += 1
+print("섭씨 온도 리스트: " + str(temperature_list))  # 섭씨 온도 출력
