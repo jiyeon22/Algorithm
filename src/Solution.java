8f6dc6690fde3fea6898ce3fb5d2893f17f90d82
jiyeon22 <kkll4106@naver.com>
2022-10-24 13:42:36 +0900

    다시 커

--- src/Solution.java
+++ src/Solution.java
@@ -5,7 +5,8 @@ import java.util.Comparator;
 import java.util.Iterator;
 import java.util.ListIterator;
 //프로그래머스 lv1. 두 개 뽑아서 더하기 
-/*2022_1018
+/*
+ //*2022_1018_
  * 정수 배열 numbers가 주어집니다. 
  * numbers에서 서로 다른 인덱스에 있는 두 개의 수를 뽑아 더해서 만들 수 있는 모든 수를 
  * 배열에 오름차순으로 담아 return 하도록 solution 함수를 완성해주세요.
