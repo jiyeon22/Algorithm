fd4ed1acc7767d61d5d9e70397638169ba2e4d21
Jiyeon <kkll4106@naver.com>
2025-04-17 22:06:00 +0900

    update: modify extract_vowels and update READEME

--- python/문자열/extract_vowels.py
+++ python/문자열/extract_vowels.py
@@ -1,23 +1,15 @@
-{
- "cells": [
-  {
-   "cell_type": "code",
-   "execution_count": null,
-   "id": "f1089bc7-ad82-47b5-b08d-447fc23af362",
-   "metadata": {},
-   "outputs": [],
-   "source": []
-  }
- ],
- "metadata": {
-  "kernelspec": {
-   "display_name": "",
-   "name": ""
-  },
-  "language_info": {
-   "name": ""
-  }
- },
- "nbformat": 4,
- "nbformat_minor": 5
-}
+N = int(input())
+
+str_list = [] 
+vowels = ['a','e','i','o','u']
+
+for i in range(N):
+	s = input()
+	str_list.append(s)
+	
+for word in str_list:
+	only_vowels = [ch for ch in word if ch.lower() in vowels]
+	if only_vowels:
+		print(''.join(only_vowels))
+	else:
+		print("???")
